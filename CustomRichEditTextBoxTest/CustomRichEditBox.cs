aea0f71a94373a0017cdae178af341dfad9f46a4
v-zhumin <v-zhumin@V-ZHUMIN-W10>
2017-04-10 18:56:41 +0800

    Add project files.

---
+++ CustomRichEditTextBoxTest/CustomRichEditBox.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+// The Templated Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace CustomRichEditTextBoxTest
+{
+    public sealed class CustomRichEditBox : RichEditBox
+    {
+        public CustomRichEditBox()
+        {
+            this.DefaultStyleKey = typeof(RichEditBox);
+            this.TextChanged += CustomRichEditBox_TextChanged;
+        }
+
+        private void CustomRichEditBox_TextChanged(object sender, RoutedEventArgs e)
+        {
+            string value = string.Empty;
+            this.Document.GetText(Windows.UI.Text.TextGetOptions.AdjustCrlf, out value);
+            if (string.IsNullOrEmpty(value))
+            {
+                return;
+            }
+            CustomText = value;
+        }
+
+        public string CustomText
+        {
+            get { return (string)GetValue(CustomTextProperty); }
+            set
+            {
+                SetValue(CustomTextProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty CustomTextProperty =
+            DependencyProperty.Register("CustomText", typeof(string), typeof(CustomRichEditBox), new PropertyMetadata(null, new PropertyChangedCallback(OnCustomTextChanged)));
+
+        private static void OnCustomTextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            CustomRichEditBox rich = d as CustomRichEditBox;
+            if (e.NewValue != e.OldValue)
+            {
+                rich.Document.SetText(Windows.UI.Text.TextSetOptions.None, e.NewValue.ToString());
+            }
+        }
+    }
+}
---
+++ CustomRichEditTextBoxTest/ViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomRichEditTextBoxTest
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private string _info;
+
+        public string Info
+        {
+            get { return _info; }
+            set
+            {
+                _info = value;
+
+                RaisePropertyChanged("Info");
+            }
+        }
+
+        public ViewModel()
+        {
+            _info = "test binding";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
